e1c73ddc2faa5a472e082018d844efa4cfca11ff
Josh <liljoshf@yahoo.com>
2021-08-16 17:53:37 -0400

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(names) {
+    const namesarray = []
+    for (let i = 0; i < names.length; i++) {
+        namesarray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+      }   
+      return namesarray
+    }
+
+    function countDown(numbers) {
+        for (let i = numbers; i >=0; i--) {
+        console.log(i)
+        }   
+  }
+        
